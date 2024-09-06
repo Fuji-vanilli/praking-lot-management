cd27a71056171aaa5cb4887b17b94af7d050dc0e
Fuji-vanilli <ijufillivan@gmail.com>
2024-09-06 11:28:56 +0300

    update

--- src/main/java/org/fuji/entities/ParkingLot.java
+++ src/main/java/org/fuji/entities/ParkingLot.java
@@ -3,11 +3,35 @@ package org.fuji.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Data
-@AllArgsConstructor
 public class ParkingLot {
     private String parkingLotId;
     private List<List<Slot>> slots;
+
+    public ParkingLot(int numberFloors, int numberOfSlotPerFloor) {
+        this.parkingLotId= UUID.randomUUID().toString();
+        this.slots= new ArrayList<>();
+
+        for (int i= 0; i< numberFloors; i++) {
+            List<Slot> floorSlots= List.of(new Slot("bike"), new Slot("moto"), new Slot("truck"));
+
+            slots.add(floorSlots);
+            for (int j= 3; j< numberOfSlotPerFloor; j++) {
+                slots.get(i).add(new Slot("car"));
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/org/fuji/entities/Slot.java
+++ src/main/java/org/fuji/entities/Slot.java
@@ -4,9 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Slot {
     private String type;
     private Vehicle vehicle;
     private String ticketId;
+
+    public Slot(String type) {
+        this.type= type;
+    }
+
+    public Slot(String type, Vehicle vehicle) {
+        this.type= type;
+        this.vehicle= vehicle;
+    }
 }
